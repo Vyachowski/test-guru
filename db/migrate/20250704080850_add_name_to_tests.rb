5f453d053de951132bc7a03eec5125c2492ed714
Slava Haikin <vyachowski@gmail.com>
2025-07-05 14:54:11 +0300

    refactor: delete excess migration for tests

--- db/migrate/20250704080850_add_name_to_tests.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToTests < ActiveRecord::Migration[7.2]
-  def change
-    add_column :tests, :name, :string, null: false
-    add_column :tests, :creator_id, :bigint, null: false
-    add_foreign_key :tests, :users, column: :creator_id
-  end
-end
