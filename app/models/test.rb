92dad5d992da7247692df7384bdcac8bc897d065
Slava Haikin <vyachowski@gmail.com>
2025-07-06 17:39:56 +0300

    feat: add class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,12 @@
 class Test < ApplicationRecord
+  def self.find_all_by_category_name(category_name)
+    joins("INNER JOIN categories ON categories.id = tests.category_id")
+      .where(categories: { title: category_name })
+      .order(name: :desc)
+      .pluck(:name)
+  end
 end
+
+# test-guru(dev)> Test.find_all_by_category_name(:Frontend)
+#   Test Pluck (0.6ms)  SELECT "tests"."name" FROM "tests" INNER JOIN categories ON categories.id = tests.category_id WHERE "categories"."title" = ? ORDER BY "tests"."name" DESC  [["title", "Frontend"]]
+# => ["Frontend Intermediate", "Frontend Basics"]
