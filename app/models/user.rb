6c872c283462019a853430a7a9bc21a1d9f3976e
Slava Haikin <vyachowski@gmail.com>
2025-07-10 23:10:41 +0300

    refactor: simplify user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_passages
 
   def tests_by_difficulty(difficulty_level)
-    Test.joins("INNER JOIN test_passages ON test_passages.test_id = tests.id")
-        .where(test_passages: { user_id: id }, tests: { level: difficulty_level })
+    tests.where(level: difficulty_level)
   end
 end
