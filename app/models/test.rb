d79e4067b55a2d6bbb3df8e9ad07ffb54dff5203
Slava Haikin <vyachowski@gmail.com>
2025-07-10 19:03:18 +0300

    feat: add creator assosiation

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+
   def self.find_all_by_category_name(category_name)
     joins("INNER JOIN categories ON categories.id = tests.category_id")
       .where(categories: { title: category_name })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :created_tests, class_name: 'Test', foreign_key: :creator_id
+
   def tests_by_difficulty(difficulty_level)
     Test.joins("INNER JOIN test_passages ON test_passages.test_id = tests.id")
         .where(test_passages: { user_id: id }, tests: { level: difficulty_level })
