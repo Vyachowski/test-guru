ee4ba6b7417a40b02273db7f1139958b9d1d0172
Slava Haikin <vyachowski@gmail.com>
2025-07-10 19:06:26 +0300

    feat: add category association

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,9 @@
 class Test < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id
+  belongs_to :category
 
   def self.find_all_by_category_name(category_name)
-    joins("INNER JOIN categories ON categories.id = tests.category_id")
+    joins(:category)
       .where(categories: { title: category_name })
       .order(name: :desc)
       .pluck(:name)
