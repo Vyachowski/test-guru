bd027834911d30c6b8afbd1a54fdc772b3562686
Slava Haikin <vyachowski@gmail.com>
2025-07-10 19:23:54 +0300

    feat: add join table

---
+++ db/migrate/20250710161621_create_tests_passages_join_table.rb
@@ -0,0 +1,7 @@
+class CreateTestsPassagesJoinTable < ActiveRecord::Migration[7.2]
+  def change
+    create_join_table :users, :tests do |t|
+      t.index [:user_id, :test_id], unique: true
+    end
+  end
+end
