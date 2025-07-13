626c58fca5946553695dff954808ef8771e6ed43
Slava Haikin <vyachowski@gmail.com>
2025-07-13 21:47:31 +0300

    chore: apply macro records styleguide rule

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id
+
   has_many :questions
   has_many :users, through: :tests_passages
-  belongs_to :creator, class_name: "User", foreign_key: :creator_id
-  belongs_to :category
 
   def self.find_all_by_category_name(category_name)
     joins(:category)
