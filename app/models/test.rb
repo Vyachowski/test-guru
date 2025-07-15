9908f7221732c6db969e978c7868396cd580cc13
Slava Haikin <vyachowski@gmail.com>
2025-07-15 22:44:21 +0300

    chore: align scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,9 +6,10 @@ class Test < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :users, through: :test_passages
 
-  scope :easy, -> { where(level: 0..1) }
+  scope :easy,   -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
-  scope :hard, -> { where(level: 5..FLOAT::INFINITY) }
+  scope :hard,   -> { where(level: 5..Float::INFINITY) }
+
   scope :by_category_name, ->(name) {
     joins(:category).where(categories: { title: name }).order(name: :desc)
   }
