10376da6a474a374d0fe8e9c927fb61a52e512c8
Slava Haikin <vyachowski@gmail.com>
2025-07-18 16:58:50 +0300

    feat: add valid method

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,24 @@ class Answer < ApplicationRecord
   validates :body, presence: true
 
   scope :correct, -> { where(correct: true) }
+
+  validate :validate_answers_limit
+
+  private
+
+    def validate_answers_limit
+      return unless question
+
+      current_answers = question.answers.reject do |a|
+        a.marked_for_destruction? || a == self
+      end
+
+      total = current_answers.size + (new_record? ? 1 : 0)
+
+      if total > 4
+        errors.add(:base, "A question cannot have more than 4 answers")
+      elsif total < 1
+        errors.add(:base, "A question must have at least one answer")
+      end
+    end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,18 +2,4 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
-
-  validate :validate_answers_limit
-
-  private
-
-    def validate_answers_limit
-      size = answers.reject(&:marked_for_destruction?).size
-
-      if size < 1
-        errors.add(:answers, "should have at least one answer")
-      elsif size > 4
-        errors.add(:answers, "cannot have more than 4 answers")
-      end
-    end
 end
