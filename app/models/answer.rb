aff9e96823b51572ca9ecb2bb4e936ac7427f365
Slava Haikin <vyachowski@gmail.com>
2025-07-20 18:24:52 +0300

    fix: add correct answers checking

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -11,16 +11,8 @@ class Answer < ApplicationRecord
     def validate_answers_limit
       return unless question
 
-      current_answers = question.answers.reject do |a|
-        a.marked_for_destruction? || a == self
-      end
-
-      total = current_answers.size + (new_record? ? 1 : 0)
-
-      if total > 4
+      if question.answers.count >= 4 && new_record?
         errors.add(:base, "A question cannot have more than 4 answers")
-      elsif total < 1
-        errors.add(:base, "A question must have at least one answer")
       end
     end
 end
