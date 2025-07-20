9491510408b9de8ecf2db47574f63c8428e9876a
Slava Haikin <vyachowski@gmail.com>
2025-07-20 18:45:06 +0300

    refactor: add simplified validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_answers_limit
+  validate :validate_answers_limit, on: :create
 
   scope :correct, -> { where(correct: true) }
 
@@ -11,7 +11,7 @@ class Answer < ApplicationRecord
     def validate_answers_limit
       return unless question
 
-      if question.answers.count >= 4 && new_record?
+      if question.answers.count >= 4
         errors.add(:base, "A question cannot have more than 4 answers")
       end
     end
