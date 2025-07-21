b96d141caea406578b4b7bb080939347905e0f9e
Slava Haikin <vyachowski@gmail.com>
2025-07-22 00:23:37 +0300

    feat: add question method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,22 @@
 class QuestionsController < ApplicationController
   before_action :find_test
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   def index
     render json: @test.questions
   end
 
+  def show
+    render json: @test.questions.find(params[:id])
+  end
+
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  private
+
+  def render_not_found
+    render json: { errors: "Record not found" }, status: :not_found
   end
 end
