df6e7761c87e9428ebda911a88ca2f03e92a0c7e
Slava Haikin <vyachowski@gmail.com>
2025-07-22 00:16:37 +0300

    feat: add index method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,10 @@
 class QuestionsController < ApplicationController
   before_action :find_test
 
+  def index
+    render json: @test.questions
+  end
+
   def find_test
     @test = Test.find(params[:test_id])
   end
