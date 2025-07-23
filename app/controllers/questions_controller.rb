31f8f0dd0bae9eb334b1ee87a6ae746f35e5da51
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2025-07-23 12:08:05 +0300

    [FEAT] Добавляет контроллеры для вопросов (#12)
    * feat: add test/questions resources
    
    * feat: add callback
    
    * feat: add index method
    
    * feat: add question method
    
    * feat: add additional method for question
    
    * feat: add additional method for question
    
    * feat: add new methods for form creation
    
    * feat: add form template
    
    * feat: add connected form
    
    * feat: add question validation, add empty form checking, add question controller
    
    * feat: disable turbo form behaviour, add error status
    
    * feat: add destroy method
    
    * feat: add newline in html template
    
    * feat: add shallow routing with simplified methods

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,48 @@
+class QuestionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  before_action :set_test, only: [:index, :new, :create]
+  before_action :set_question, only: [:show, :destroy]
+
+  def index
+    render json: @test.questions
+  end
+
+  def show
+    render json: @question
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = @test.questions.new(params.require(:question).permit(:body))
+
+    if @question.save
+      redirect_to test_question_path(@test, @question), notice: "Вопрос успешно создан"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to questions_path, notice: "Вопрос удалён"
+  end
+
+  private
+
+  def set_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def render_not_found
+    render json: { error: "Record not found" }, status: :not_found
+  end
+end
