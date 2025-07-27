3173fd67a080d2a1a3840472b5a69aaf85cf53f0
Slava Haikin <vyachowski@gmail.com>
2025-07-27 22:56:45 +0300

    chore: delete excess files; add test level helper

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,12 @@
 module TestsHelper
+  def test_level(test)
+    levels = {
+      0 => "easy",
+      1 => "elementary",
+      2 => "advanced",
+      3 => "hard"
+    }
+
+    levels.fetch(test.level, "hero")
+  end
 end
