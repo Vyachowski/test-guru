91065972f725f5922e0a4ba68a84eb159292217b
Slava Haikin <vyachowski@gmail.com>
2025-07-28 14:14:59 +0300

    refactor: add simplified level helper

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,13 +1,13 @@
 module TestsHelper
-  def test_level(test)
-    levels = {
-      0 => "easy",
-      1 => "elementary",
-      2 => "advanced",
-      3 => "hard"
+  LEVELS = {
+    0 => "easy",
+    1 => "elementary",
+    2 => "advanced",
+    3 => "hard"
   }.freeze
 
-    levels.fetch(test.level, "hero")
+  def test_level(test)
+    LEVELS.fetch(test.level, "hero")
   end
 
   def test_header(test_title, is_edit_form = false)
