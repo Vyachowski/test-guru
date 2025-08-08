f1f43557fe04a03d0b281d5c8ec2c74b61924994
Slava Haikin <vyachowski@gmail.com>
2025-08-08 21:13:16 +0300

    feat: apply db migration and remove not null constraint

--- db/migrate/20250808180551_add_current_question_correct_questions_to_test_passages.rb
+++ db/migrate/20250808180551_add_current_question_correct_questions_to_test_passages.rb
@@ -1,6 +1,6 @@
 class AddCurrentQuestionCorrectQuestionsToTestPassages < ActiveRecord::Migration[7.2]
   def change
-    add_reference :test_passages, :current_question, null: false, foreign_key: { to_table: :questions }
+    add_reference :test_passages, :current_question, null: true, foreign_key: { to_table: :questions }
     add_column :test_passages, :correct_questions, :integer, null: false, default: 0
 
     remove_column :test_passages, :passed, :boolean
