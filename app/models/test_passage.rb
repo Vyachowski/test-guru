b82ec55b4a33fe3dcaf956e3a77eba77703b3ed8
Slava Haikin <vyachowski@gmail.com>
2025-08-10 20:50:18 +0300

    fix: add array checking

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -25,6 +25,7 @@ class TestPassage < ApplicationRecord
   private
 
   def correct_answer?(answer_ids)
+    answer_ids = Array(answer_ids).reject(&:blank?)
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
