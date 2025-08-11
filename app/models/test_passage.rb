31d454003e8eeae8ff7241e476a024e8670e2b8d
Slava Haikin <vyachowski@gmail.com>
2025-08-11 12:20:26 +0300

    refactor: simplify sorting method

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -35,8 +35,7 @@ class TestPassage < ApplicationRecord
   private
 
   def correct_answer?(answer_ids)
-    answer_ids = Array(answer_ids).reject(&:blank?)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   def correct_answers
