d70ac7780792743695f2549dffa580461b1ffc0d
Slava Haikin <vyachowski@gmail.com>
2025-08-13 20:09:08 +0300

    feat: add actions skipping for signup/signin methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+
+  def new; end
+
   def create
     user = User.find_by(email: params[:email])
 
