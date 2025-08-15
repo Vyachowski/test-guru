fc402284f4dd4f8cebdaec89287b275c9eed0f1b
Slava Haikin <vyachowski@gmail.com>
2025-08-15 17:27:59 +0300

    chore: delete sign in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  get :login, to: "sessions#new"
-  get :signup, to: "users#new"
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   delete :logout, to: "sessions#destroy"
-
-  resources :users, only: :create
-  resources :sessions, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
