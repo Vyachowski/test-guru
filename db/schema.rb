cd514115752c97f7ba8400ef568b6c607c199d02
Slava Haikin <vyachowski@gmail.com>
2025-09-10 01:58:27 +0300

    feat: add user name and type migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_15_111300) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_09_225613) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
@@ -70,6 +70,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_15_111300) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "type", default: "User", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
