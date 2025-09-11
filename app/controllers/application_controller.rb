82bd135cda31f229d291cb58917d06e37095f010
Slava Haikin <vyachowski@gmail.com>
2025-09-11 15:50:50 +0300

    feat: add admin redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   def redirect_back_or_to_root
     redirect_to(cookies.delete(:redirect_to) || root_path)
   end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_tests_path
+    else
+      super(resource)
+    end
+  end
 end
