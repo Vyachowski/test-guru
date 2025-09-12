0b11b732f8497b4a79a841c860f113b13ea07295
Slava Haikin <vyachowski@gmail.com>
2025-09-12 15:46:14 +0300

    fix: update route separation between admin and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :tests do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
-
+  resources :tests, only: [:index, :show] do
     post :start, on: :member
+
+    resources :questions, only: [:show], shallow: true do
+      resources :answers, only: [:show], shallow: true
+    end
   end
 
   resources :test_passages, only: %i[show update] do
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :tests
+    resources :tests do
+      resources :questions, shallow: true do
+        resources :answers, shallow: true
+      end
+    end
   end
 
   root "tests#index"
