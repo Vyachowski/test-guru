7105ea65d218a72a915e7e964b1dbad47018842f
Slava Haikin <vyachowski@gmail.com>
2025-09-14 23:03:08 +0300

    feat: add separate admin answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,49 +1,5 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: %i[show edit update destroy]
-  before_action :find_question, only: %i[new create]
-
-  def show; end
-
-  def new
-    @answer = @question.answers.new
-  end
-
-  def create
-    @answer = Answer.new(answer_params)
-
-    if @answer.save
-      redirect_to @answer, notice: "Ответ успешно создан."
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @answer.update(answer_params)
-      redirect_to @answer, notice: "Ответ успешно обновлён."
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @answer.destroy
-    redirect_to @answer.question
-  end
-
-  private
-
-  def set_answer
+  def show
     @answer = Answer.find(params[:id])
-  end
-
-  def answer_params
-    params.require(:answer).permit(:body, :correct)
-  end
-
-  def find_question
-    @question = Question.find(params[:question_id])
   end
 end
