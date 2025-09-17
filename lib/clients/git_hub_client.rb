61dd50dd65f78e7a670f2cec57c1b78cd580deec
Slava Haikin <vyachowski@gmail.com>
2025-09-17 21:34:11 +0300

    feat: add credentials and client file

---
+++ lib/clients/git_hub_client.rb
@@ -0,0 +1,22 @@
+class GitHubClient
+  ROOT_ENDPOINT = "https://api.github.com"
+  GIST_AUTH_TOKEN = Rails.application.credentials.gist_token
+
+  def initialize
+    @http_client = setup_http_client
+  end
+
+  def create_gist(params)
+    @http_client.post("gists") do |request|
+      request.headers["Authorization"] = "token #{GIST_AUTH_TOKEN}"
+      reques.headers["Content-Type"] = "application/json"
+      request.body = params.to_json
+    end
+  end
+
+  private
+
+  def setup_http_client
+    Faraday.new(url: ROOT_ENDPOINT)
+  end
+end
