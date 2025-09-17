6efb9e7854ecff5e48c278a7fc708dda1b97d1f2
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2025-09-17 17:56:09 +0300

    [FEAT] Add bootsrap framework (#31)
    * feat: add bootstrap gem
    
    * feat: add bootstrap files
    
    * feat: add user navbar
    
    * feat: add bootstrap class helpers
    
    * feat: add styled footer
    
    * feat: add user tests list styling
    
    * feat: add user tests additional styling
    
    * feat: add admin navigation
    
    * feat: add styling to admin tests
    
    * feat: add styling to admin questions/answers
    
    * feat: add new question link on test page
    
    * feat: add form styling
    
    * feat: add login/signup styles
    
    * feat: add restore password stying
    
    * feat: add password confirmation stying
    
    * feat: add correct nav translations
    
    * fix: add correct test passage logic
    
    * chore: add file newline at the end
    
    * chore: fix variable name
    
    * chore: fix translations

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "bootsnap", require: false
 gem "sassc-rails", "~> 2.1"
 gem "devise", "~> 4.9"
 gem "rails-i18n", "~> 7.0.9"
+gem "bootstrap", "~> 5.3.5"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,26 @@ module ApplicationHelper
     Time.current.year
   end
 
-  def github_url(author, repo)
-    link_to repo.tr("-", " ").titleize, "https://github.com/#{author}/#{repo}", target: "_blank", rel: "noopener"
+  def github_url(author, repo, class_names)
+    link_to repo.tr("-", " ").titleize, "https://github.com/#{author}/#{repo}", target: "_blank", rel: "noopener", class: class_names
   end
 
   def flash_message(type)
     if flash[type].present?
       content_tag :p, flash[type], class: "flash #{type}"
+    end
+  end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type.to_sym
+    when :notice
+      "alert alert-success"
+    when :alert
+      "alert alert-danger"
+    when :error
+      "alert alert-danger"
+    else
+      "alert alert-primary"
     end
   end
 end
