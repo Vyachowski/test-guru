55430270ea4badc3d091eccc66703d2b5cf23cb6
Slava Haikin <vyachowski@gmail.com>
2025-09-18 16:48:45 +0300

    feat: add model saving on response

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -17,11 +17,16 @@ class TestPassagesController < ApplicationController
 
   def gist
     result = GistQuestionService.new(@test_passage.current_question).call
+    response_body = JSON.parse(result.env.response_body)
+    gist_url = response_body["html_url"]
+    question_body = @test_passage.current_question.body
 
-    flash_options = if result.success?
-      { notice: t(".success") }
+    new_gist = Gist.create(question: question_body, url: gist_url, user: current_user)
+
+    if result.success? && new_gist.persisted?
+      flash_options = { notice: t(".success") }
     else
-      { alert: t(".failure") }
+      flash_options = { alert: t(".failure") }
     end
 
     redirect_to @test_passage, **flash_options
--- app/models/gist.rb
+++ app/models/gist.rb
@@ -2,5 +2,5 @@ class Gist < ApplicationRecord
   belongs_to :user
 
   validates :question, presence: true
-  validate :url, presence: true
+  validates :url, presence: true
 end
