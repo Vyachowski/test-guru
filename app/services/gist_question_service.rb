4498f0bfbdb4975f3a45785b5424e0a41cf60bfd
Slava Haikin <vyachowski@gmail.com>
2025-09-18 15:19:21 +0300

    feat: add service internationalization

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -13,7 +13,10 @@ class GistQuestionService
 
   def gist_params
     {
-      description: "A question about #{@test.title} from TestGuru",
+      description: I18n.t(
+        "test_passages.gist.description",
+        title: @test.title
+      ),
       files: {
         "test-guru-question.txt" => {
           content: gist_content
