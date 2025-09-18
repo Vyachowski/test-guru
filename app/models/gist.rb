9e1294bd0a31935f4ad4f833c315cb81e9f20262
Slava Haikin <vyachowski@gmail.com>
2025-09-18 18:44:06 +0300

    feat: add model url validator

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -2,5 +2,5 @@ class Gist < ApplicationRecord
   belongs_to :user
 
   validates :question, presence: true
-  validates :url, presence: true
+  validates :url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 end
