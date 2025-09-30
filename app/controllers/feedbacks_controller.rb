42c2bfd3cfc748ffc6fb9a585262d7ff2961ca27
Slava Haikin <vyachowski@gmail.com>
2025-10-01 00:15:23 +0300

    feat: add controller methods, form translations

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -4,5 +4,15 @@ class FeedbacksController < ApplicationController
   end
 
   def create
+    feedback_params = params.require(:feedback).permit(:name, :email, :body)
+    @feedback = Feedback.new(feedback_params)
+
+    if @feedback.valid?
+      FeedbackMailer.feedback_email(@feedback).deliver_now
+      redirect_to new_feedback_path, notice: "Сообщение отправлено!"
+    else
+      flash.now[:alert] = "Заполните все поля"
+      render :new, status: :unprocessable_entity
+    end
   end
 end
