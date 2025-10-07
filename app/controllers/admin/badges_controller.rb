838bea7e93b518489933167c96591556b3593c41
Slava Haikin <vyachowski@gmail.com>
2025-10-07 03:01:40 +0300

    feat: add admin routes and controller

---
+++ app/controllers/admin/badges_controller.rb
@@ -0,0 +1,29 @@
+class Admin::BadgesController < ApplicationController
+  before_action :set_badge, only: %i[show update]
+
+  def index
+    @badges = Badge.all
+  end
+
+  def show; end
+
+  def update
+    if @badge.update(badge_params)
+      notice_message = @badge.active ? t("badges.activated") : t("badges.deactivated")
+
+      redirect_to admin_badge_path(@badge), notice: notice_message
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:active)
+  end
+end
