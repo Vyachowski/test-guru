f08479a33fc68f6cfcd7945af993771ec4720642
Slava Haikin <vyachowski@gmail.com>
2025-10-08 18:31:04 +0300

    feat: add badge service

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -9,6 +9,8 @@ class TestPassagesController < ApplicationController
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
+      BadgeService.new(@test_passage.user, @test_passage).call
+
       redirect_to result_test_passage_path(@test_passage)
     else
       redirect_to test_passage_path(@test_passage)
---
+++ app/services/badge_service.rb
@@ -0,0 +1,39 @@
+class BadgeService
+  def initialize(user, test_passage)
+    @user = user
+    @test_passage = test_passage
+  end
+
+  def call
+    Badge.find_each do |badge|
+      give_badge(badge) if send("#{badge.rule_type}?", badge.rule_value)
+    end
+  end
+
+  private
+
+  def give_badge(badge)
+    @user.badges << badge
+  end
+
+  def all_in_category?(category_name)
+    category = @test_passage.test.category
+    return false unless category.name == category_name
+
+    category_tests = category.tests.ids
+    passed_tests = @user.test_passages.where(successful: true).pluck(:test_id).uniq
+    (category_tests - passed_tests).empty?
+  end
+
+  def first_try?(_value = nil)
+    @user.test_passages.where(test: @test_passage.test).count == 1 && @test_passage.successful?
+  end
+
+  def all_of_level?(level)
+    return false unless @test_passage.test.level == level.to_i
+
+    level_tests = Test.where(level: level).ids
+    passed_tests = @user.test_passages.where(successful: true).pluck(:test_id).uniq
+    (level_tests - passed_tests).empty?
+  end
+end
