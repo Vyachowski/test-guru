24686457792de086a541f712362859992b558c0b
Slava Haikin <vyachowski@gmail.com>
2025-10-12 21:53:21 +0300

    fix: restore test passage redirect

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -14,6 +14,8 @@ class TestPassagesController < ApplicationController
       flash[:notice] = "Вы получили новый бэйдж: #{new_badges.map(&:name).join(', ')}" if new_badges.any?
 
       redirect_to result_test_passage_path(@test_passage)
+    else
+      redirect_to test_passage_path(@test_passage)
     end
   end
 
